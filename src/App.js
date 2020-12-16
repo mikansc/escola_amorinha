79d9d444cbb3a08525da8ab8fd4204950fad2f99
mikansc <michael.nsc@outlook.com>
2020-12-16 09:11:29 -0300

    refactor: create placeholders to test the design

--- src/App.js
+++ src/App.js
@@ -9,36 +9,81 @@ function App() {
       <section className="container m-3 p-2 border">
         <h2 className="h5">Formulário de cadastro</h2>
         <form>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <small id="emailHelp" className="form-text text-muted">
-              We will never share your email with anyone else.
-            </small>
+          <div className="form-row">
+            <div className="form-group col-md-8">
+              <label htmlFor="inputCity">Nome do aluno</label>
+              <input type="text" className="form-control" id="inputCity" />
+            </div>
+
+            <div className="form-group col-md-4">
+              <label htmlFor="inputZip">Data de Nascimento</label>
+              <input type="text" className="form-control" id="inputZip" />
+            </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-            />
+
+          <div className="form-row">
+            <div className="form-group col-md-8">
+              <label htmlFor="inputCity">City</label>
+              <input type="text" className="form-control" id="inputCity" />
+            </div>
+
+            <div className="form-group col-md-4">
+              <label htmlFor="inputZip">Zip</label>
+              <input type="text" className="form-control" id="inputZip" />
+            </div>
           </div>
-          <div className="form-group form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-            />
-            <label className="form-check-label" htmlFor="exampleCheck1">
-              Check me out
-            </label>
+
+          <div className="form-row">
+            <div className="form-group col-md-4">
+              <label htmlFor="inputCity">City</label>
+              <input type="text" className="form-control" id="inputCity" />
+            </div>
+
+            <div className="form-group col-md-4">
+              <label htmlFor="inputZip">Zip</label>
+              <input type="text" className="form-control" id="inputZip" />
+            </div>
+
+            <div className="form-group col-md-4">
+              <label htmlFor="inputZip">Zip</label>
+              <input type="text" className="form-control" id="inputZip" />
+            </div>
           </div>
+
+          <div className="form-row">
+            <div className="form-group col-md-4">
+              <label htmlFor="inputCity">City</label>
+              <input type="text" className="form-control" id="inputCity" />
+            </div>
+
+            <div className="form-group col-md-8">
+              <label htmlFor="inputZip">Zip</label>
+              <input type="text" className="form-control" id="inputZip" />
+            </div>
+          </div>
+
+          <div className="form-row">
+            <div className="col-md-4 my-1">
+              <div className="custom-control custom-checkbox mr-sm-2">
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="customControlAutosizing"
+                />
+                <label
+                  className="custom-control-label"
+                  htmlFor="customControlAutosizing"
+                >
+                  Remember my preference
+                </label>
+              </div>
+            </div>
+            <div className="form-group col-md-8">
+              <label htmlFor="inputZip">Zip</label>
+              <input type="text" className="form-control" id="inputZip" />
+            </div>
+          </div>
+
           <button type="submit" className="btn btn-primary">
             Submit
           </button>
