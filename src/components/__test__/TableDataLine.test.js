b481387a8116fda5ff760d0b5b598cbb06c5850c
mikansc <michael.nsc@outlook.com>
2020-12-16 14:13:11 -0300

    test: should render a table row element

--- src/components/__test__/TableDataLine.test.js
+++ src/components/__test__/TableDataLine.test.js
@@ -1,12 +1,17 @@
-import { render } from "@testing-library/react";
 import React from "react";
+import { render, screen } from "@testing-library/react";
 
 const TableDataLine = () => {
-  return <div></div>;
+  return <tr></tr>;
 };
 
 describe("Table Data Line component", () => {
   test("should render TableDataLine correctly", () => {
     render(<TableDataLine />);
+  });
+
+  test("should render a table row element", () => {
+    render(<TableDataLine />);
+    expect(screen.getByRole("row")).toBeInTheDocument();
   });
 });
