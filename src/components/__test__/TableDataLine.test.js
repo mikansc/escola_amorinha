fe6d12647fed8ebf28dbb7179d26751870cbb590
mikansc <michael.nsc@outlook.com>
2020-12-16 12:15:46 -0300

    test: should render TableDataLine correctly

---
+++ src/components/__test__/TableDataLine.test.js
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+const TableDataLine = () => {
+  return <div></div>;
+};
+
+describe("Table Data Line component", () => {
+  test("should render TableDataLine correctly", () => {
+    render(<TableDataLine />);
+  });
+});
