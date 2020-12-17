78929c0547cd7e1a8719b1beab7563f0127a0592
mikansc <michael.nsc@outlook.com>
2020-12-17 18:30:37 -0300

    test: render all <th> with the heading text correctly

--- src/components/__test__/Table.test.js
+++ src/components/__test__/Table.test.js
@@ -5,11 +5,11 @@ const Table = () => {
   return (
     <table>
       <thead data-testid="thead">
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
+        <th>Nome</th>
+        <th>Data de Nascimento</th>
+        <th>Turma</th>
+        <th>Telefone para Emergências</th>
+        <th>Em caso de emergência avisar</th>
         <th></th>
       </thead>
     </table>
@@ -36,6 +36,20 @@ describe("Table component", () => {
 
     test("should render 6 <th> elements", () => {
       expect(screen.getAllByRole("columnheader")).toHaveLength(6);
+    });
+
+    test("should render all <th> with the heading text correctly", () => {
+      const LABELS = [
+        "Nome",
+        "Data de Nascimento",
+        "Turma",
+        "Telefone para Emergências",
+        "Em caso de emergência avisar",
+      ];
+
+      LABELS.forEach((label) => {
+        expect(screen.getByText(label)).toBeInTheDocument();
+      });
     });
   });
 });
