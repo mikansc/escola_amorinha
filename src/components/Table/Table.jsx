7e6901c815abf3c1ce63d962d917b767c1b6877b
mikansc <michael.nsc@outlook.com>
2020-12-17 19:08:23 -0300

    style: add styles to table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,15 +3,15 @@ import React from "react";
 
 const Table = ({ children }) => {
   return (
-    <table>
+    <table className="table table-hover table-default">
       <thead data-testid="thead">
         <tr>
-          <th>Nome</th>
-          <th>Data de Nascimento</th>
-          <th>Turma</th>
-          <th>Telefone para Emergências</th>
-          <th>Em caso de emergência avisar</th>
-          <th></th>
+          <th scope="col">Nome</th>
+          <th scope="col">Data de Nascimento</th>
+          <th scope="col">Turma</th>
+          <th scope="col">Telefone para Emergências</th>
+          <th scope="col">Em caso de emergência avisar</th>
+          <th scope="col"></th>
         </tr>
       </thead>
       <tbody data-testid="tbody">{children}</tbody>
