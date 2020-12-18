7ca015cbda6288c8fa37155e2c7c76cdbc9306c9
mikansc <michael.nsc@outlook.com>
2020-12-18 13:47:51 -0300

    test: render an text input element

--- src/components/__test__/Filter.test.js
+++ src/components/__test__/Filter.test.js
@@ -1,12 +1,26 @@
 import React from "react";
-const { render } = require("@testing-library/react");
+const { render, screen } = require("@testing-library/react");
 
 const Filter = () => {
-  return <p></p>;
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  );
 };
 
 describe("Filter component", () => {
   test("should render a Filter component correctly", () => {
     render(<Filter />);
+  });
+
+  describe("Filter elements", () => {
+    beforeEach(() => {
+      render(<Filter />);
+    });
+
+    test("should render an text input element", () => {
+      expect(screen.getByRole("textbox").getAttribute("type")).toBe("text");
+    });
   });
 });
