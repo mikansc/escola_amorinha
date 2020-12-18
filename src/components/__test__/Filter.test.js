6ca3e16efd1b46ec47f1911a7b7ca295dcf14f87
mikansc <michael.nsc@outlook.com>
2020-12-18 14:04:10 -0300

    test: have a placeholder with text 'buscar aluno pelo nome'"

--- src/components/__test__/Filter.test.js
+++ src/components/__test__/Filter.test.js
@@ -4,7 +4,7 @@ const { render, screen } = require("@testing-library/react");
 const Filter = () => {
   return (
     <div>
-      <input type="text" />
+      <input type="text" placeholder="Buscar aluno pelo nome..." />
     </div>
   );
 };
@@ -21,6 +21,11 @@ describe("Filter component", () => {
 
     test("should render an text input element", () => {
       expect(screen.getByRole("textbox").getAttribute("type")).toBe("text");
+    });
+
+    test("should have a placeholder with text 'buscar aluno pelo nome'", () => {
+      const PLACEHOLDER = "Buscar aluno pelo nome...";
+      expect(screen.getByPlaceholderText(PLACEHOLDER)).toBeInTheDocument();
     });
   });
 });
