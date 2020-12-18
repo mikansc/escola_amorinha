553cad6678cfe49feb1f2db70f5c9b741677a7e8
mikansc <michael.nsc@outlook.com>
2020-12-18 19:55:29 -0300

    styles: add bootstrap styles

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const Filter = () => {
   return (
-    <div>
-      <input type="text" placeholder="Buscar aluno pelo nome..." />
-      <button type="button">Buscar</button>
+    <div className="form-inline">
+      <input
+        className="form-control mr-2 my-4  w-50"
+        type="text"
+        placeholder="Buscar aluno pelo nome..."
+      />
+      <button className="btn btn-outline-success my-2 my-sm-0" type="button">
+        Buscar
+      </button>
     </div>
   );
 };
