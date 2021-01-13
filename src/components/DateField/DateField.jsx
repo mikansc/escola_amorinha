5fef805096fc159eef90171f335a51c7927197c6
mikansc <michael.nsc@outlook.com>
2021-01-13 19:58:17 -0300

    feat: [SP05] 13 - Inclusão de campos com máscara

--- src/components/DateField/DateField.jsx
+++ src/components/DateField/DateField.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 
-const DateField = ({ title = "Data", id }) => {
+const DateField = ({ title = "Data", id, ...props }) => {
   return (
     <div className="form-group col-md-4">
       <label htmlFor={id}>{title}</label>
-      <input type="date" className="form-control" id={id} />
+      <input type="date" className="form-control" id={id} {...props} />
     </div>
   );
 };
