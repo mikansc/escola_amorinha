d9d4ecdabb70cc787ba492c1a988d40158cc8605
mikansc <michael.nsc@outlook.com>
2021-01-27 12:28:11 -0300

    refactor: change filter component to handle external props

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Filter = () => {
+const Filter = ({ onFilter, isLoading = false }) => {
+  const [term, setTerm] = useState("");
+
+  const handleOnChange = (value) => {
+    onFilter(value);
+    setTerm(value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Pesquisar", term);
+    onFilter(term);
+  };
+
+  const handleOnFocus = () => {
+    setTerm("");
+  };
+
   return (
-    <div className="form-inline">
+    <form className="form-inline" onSubmit={(e) => handleSubmit(e)}>
       <input
         className="form-control mr-2 my-4  w-50"
         type="text"
         placeholder="Buscar aluno pelo nome..."
+        onChange={(e) => handleOnChange(e.target.value)}
+        onFocus={handleOnFocus}
+        value={term}
       />
-      <button className="btn btn-outline-success my-2 my-sm-0" type="button">
-        Buscar
-      </button>
-    </div>
+      {isLoading && (
+        <div className="spinner-border text-primary" role="status">
+          <span className="visually-hidden"></span>
+        </div>
+      )}
+    </form>
   );
 };
 
